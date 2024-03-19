4e87503595379c3a7960a6ea845b32a906c07c42
jadna <jadna.ac@gmail.com>
2024-03-19 17:50:49 +0000

    ajustando o scraping para filtar melhor os dados

--- scraping_reddit.py
+++ scraping_reddit.py
@@ -15,56 +15,68 @@ reddit = praw.Reddit(
                         password=os.environ['PASSWORD']
                     )  
 reddit.read_only = True      
-#print(reddit.read_only)
+
 posts_list = []
 
-posts = reddit.subreddit('sustainability').hot(limit=15)
-#posts = reddit.subreddit("sustainability").top(time_filter="all")
-for post in posts:
-    posts_dict = {}
+def get_reddit(subreddit, query, sort, time, limit):
 
-    # Title of each post
-    posts_dict["Title"] = post.title
-     
-    # Text inside a post
-    posts_dict["Post Text"] = post.selftext
-     
-    # Unique ID of each post
-    posts_dict["ID"] = post.id
-     
-    # The score of a post
-    posts_dict["Score"] = post.score
-     
-    # Total number of comments inside the post
-    posts_dict["Total Comments"] = post.num_comments
-     
-    # URL of each post
-    posts_dict["Post URL"] = post.url
+    
+    #posts = reddit.subreddit(word).hot(limit=15)
+    #posts = reddit.subreddit(word).top(time_filter="all")
+    #posts = reddit.subreddit(word).top(time_filter=time, limit=limit)
+    posts = reddit.subreddit(subreddit).search(query, sort, time)
+        
+    for post in posts:
+        
+        posts_dict = {}
 
-    print(f"id: {post.id} - Title: {post.title} - Total Comments: {post.num_comments}")
-    print(f"URL: {post.url}")
-    print(f"{post.selftext}")
-    # Get comments
-    posts_dict["Comments"] = []
-    submission = reddit.submission(id=post.id)
-    replace = submission.comments.replace_more()
-    for index, comment in enumerate(submission.comments):
-        posts_dict["Comments"].append({
-            "Comment ID":comment.id,
-            "Comment":comment.body
+        # Title of each post
+        posts_dict["Title"] = post.title
+        # Text inside a post
+        posts_dict["Post Text"] = post.selftext
+        # Unique ID of each post
+        posts_dict["ID"] = post.id   
+        # The score of a post
+        posts_dict["Score"] = post.score      
+        # Total number of comments inside the post
+        posts_dict["Total Comments"] = post.num_comments       
+        # URL of each post
+        posts_dict["Post URL"] = post.url
+
+        print(f"id: {post.id} - Title: {post.title} - Total Comments: {post.num_comments}")
+        print(f"URL: {post.url}")
+        print(f"{post.selftext}")
+
+        # Get comments
+        posts_dict["Comments"] = []
+        submission = reddit.submission(id=post.id)
+        #replace = submission.comments.replace_more()
+
+        for index, comment in enumerate(submission.comments):
+            posts_dict["Comments"].append({
+                "Comment ID":comment.id,
+                "Comment":comment.body
             })
-        # print(comment.body)
-        # print("----")
-        # print(type(comment))
-        # print("=========================================================")
-        if type(comment) == MoreComments:
-            continue
-        print(f"Comment {index} id: {comment.id} -  {comment.body}")
-    posts_list.append(posts_dict)
-    print("=========================================================")
-with open("data.json", "w") as outfile: 
-    json.dump({"posts":posts_list}, outfile)
 
-# Saving the data in a pandas dataframe     
-# top_posts = pd.DataFrame(posts_dict)
-# top_posts.to_json("../Top Posts.json", index=True)
+            if type(comment) == MoreComments:
+                continue
+
+            print(f"Comment {index} id: {comment.id} -  {comment.body}")
+        
+        posts_list.append(posts_dict)
+        print("=========================================================")
+    
+    with open("data.json", "w") as outfile: 
+        json.dump({"posts":posts_list}, outfile)
+
+
+
+if __name__ == '__main__':
+
+    subreddit = "porto"
+    query = "autocarro"
+    sort = "relevance"
+    time = "month"
+    limit = 10
+        
+    get_reddit(subreddit, query, sort, time, limit)
